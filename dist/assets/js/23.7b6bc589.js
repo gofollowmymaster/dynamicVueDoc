70d6b0b2fb0952db3807c038acb78b2808da9629
alaraveler <1950796924@qq.com>
2022-07-05 21:10:51 +0800

    npm  load

---
+++ dist/assets/js/23.7b6bc589.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{637:function(n,e,t){"use strict";t.r(e);var a={name:"baseInfoLoader",components:{},data:function(){return{}},watch:{}},c=t(26),s=Object(c.a)(a,(function(){var n=this.$createElement;return(this._self._c||n)("main",{staticClass:"  relative  page-container"})}),[],!1,null,"423fcc29",null);e.default=s.exports}}]);
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -9,6 +9,29 @@ import {
 import "dyvue2/lib/dyvue2.css"
 import 'element-ui/lib/theme-chalk/index.css';
 
+
+const dev = "//23.13.5.135:8090";  
+const amapWebKey = 'ffe3f53e63db6ca6bbe1d85264a8fc5b'
+const amapWebMapKey = '467fddcf60be0b46a5506d94cd2cc858'
+const tandiMapKey = 'b0c8343e73356d02148906ef935c9cf8'
+const amapStyleConfig={
+    zoom:15,
+    zooms:[13, 18],
+    showLabel:false,
+    expandZoomRange:false,
+    animateEnable:true,
+    jogEnable:true,
+    center:[106.559675,29.559168], 
+    labelzIndex:120,
+    lockMapBound:10000,
+    regionPath:[]
+}
+const isDebug=false
+const host = dev;
+
+ 
+
+
 export default async ({
   Vue,
       options, // 附加到根实例的一些选项
@@ -28,7 +51,17 @@ export default async ({
       Vue.use(ElementUI)
     })
     import('dyvue2').then(({default:dyPugin})=>{
-      Vue.use(dyPugin)
+      Vue.use(dyPugin, {
+        actionTip:'msg',
+        isDebug,
+        host,
+        staticHost:'http:'+host+'/file/static',
+        amapWebKey,
+        amapWebMapKey,
+        tandiMapKey,
+        amapStyleConfig,
+        regionCode:'500103000000'
+    })
     })
     import('./components/vuePlugins/index.js').then(({default:dyPugin})=>{
       Vue.use(dyPugin)
